bfb3de4dce085bef637b19fc7caa751459f7b0a9
Hemil4 <hemil.learn@gmail.com>
2023-12-20 18:45:45 +0530

    Update code

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import { useField, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
 
 const CustomInput = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
+//   const [field, meta] = useField(props);
 
   return (
-    <div className="mb-3">
-      <label htmlFor={props.id || props.name} className="form-label">
-        {label}
-      </label>
-      <input
-        className={`form-control ${meta.touched && meta.error && 'is-invalid'}`}
-        {...field}
-        {...props}
-      />
-      <ErrorMessage name={field.name} component="div" className="invalid-feedback" />
-    </div>
+    <div className="form-group">
+            <label htmlFor={props.name}>{props.text}</label>
+            <Field type={props.type} name={props.name} />
+            <ErrorMessage name={props.name} component="div" className="error-message" />
+          </div>
   );
 };
 
--- src/components/ExampleForm.js
+++ src/components/ExampleForm.js
@@ -1,60 +1,50 @@
-import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import CustomInput from './CustomInput';
-import '../App.css'; // Import the CSS file
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import CustomInput from "./CustomInput";
+import "../App.css";
 
 const ExampleForm = () => {
   const initialValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    gender: '',
+    firstName: "",
+    lastName: "",
+    email: "",
+    gender: "",
     agreeToTerms: false,
   };
 
   const validationSchema = Yup.object({
-    firstName: Yup.string().required('Required'),
-    lastName: Yup.string().required('Required'),
-    email: Yup.string().email('Invalid email address').required('Required'),
-    gender: Yup.string().required('Please select a gender'),
-    agreeToTerms: Yup.boolean().oneOf([true], 'Please agree to the terms and conditions').required('Please agree to the terms and conditions'),
+    firstName: Yup.string().min(2, "Must be at least 2 characters").required("Required"),
+    lastName: Yup.string().min(2, "Must be at least 2 characters").required("Required"),
+    email: Yup.string().email("Invalid email address").required("Required"),
+    gender: Yup.string().required("Please select a gender"),
+    agreeToTerms: Yup.boolean()
+      .oneOf([true], "Please agree to the terms and conditions")
+      .required("Please agree to the terms and conditions"),
   });
 
   const onSubmit = (values, { resetForm }) => {
-    // Handle form submission logic
     console.log(values);
 
-    // Display a success message
-    alert('Form successfully submitted!');
+    alert("Form successfully submitted!");
 
-    // Reset the form after submission
     resetForm();
   };
 
-
   return (
     <div className="container">
-      <h1 style={{ textAlign: 'center', marginBottom: '20px' }}>Form using formik validator</h1>
-      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
+        Form using formik validator
+      </h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
         <Form>
-          <div className="form-group">
-            <label htmlFor="firstName">First Name</label>
-            <Field type="text" name="firstName" />
-            <ErrorMessage name="firstName" component="div" className="error-message" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="lastName">Last Name</label>
-            <Field type="text" name="lastName" />
-            <ErrorMessage name="lastName" component="div" className="error-message" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="email">Email</label>
-            <Field type="email" name="email" />
-            <ErrorMessage name="email" component="div" className="error-message" />
-          </div>
+          <CustomInput name="firstName" text="First Name" type="text" />
+          <CustomInput name="lastName" text="Last Name" type="text" />
+          <CustomInput name="email" text="Email" type="email" />
 
           <div className="form-group">
             <label>Gender</label>
@@ -68,7 +58,11 @@ const ExampleForm = () => {
                 Female
               </label>
             </div>
-            <ErrorMessage name="gender" component="div" className="error-message" />
+            <ErrorMessage
+              name="gender"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <div className="form-group">
@@ -76,7 +70,11 @@ const ExampleForm = () => {
               <Field type="checkbox" name="agreeToTerms" />
               Agree to Terms and Conditions
             </label>
-            <ErrorMessage name="agreeToTerms" component="div" className="error-message" />
+            <ErrorMessage
+              name="agreeToTerms"
+              component="div"
+              className="error-message"
+            />
           </div>
 
           <button type="submit">Submit</button>
